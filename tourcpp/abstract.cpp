9ba6a0eae899ccd98752e245e190ad5f10f8133c
Divyesh Kanagavel <kanagdhivy@gmail.com>
2022-06-30 12:38:00 +0530

    Modified abstract class

--- tourcpp/abstract.cpp
+++ tourcpp/abstract.cpp
@@ -7,4 +7,50 @@ virtual ~Container() {};
 // an abstract class which could be used as a base class for containers like vectors etc
 // = 0 means that it is a pure virtual function and the function must be implemented in the derived class.
 
+//hierarchies
+
+class Shape{
+    public:
+    virtual Point center() const = 0; // pure virtual function
+    virtual void move(Point) = 0;
+    virtual void draw() const = 0;
+    virtual void rotate(int angle) = 0;
+    virtual ~Shape(){}
+
+};
+
+class Circle : public Shape{
+    public:
+    Circle(Point p, int rr);
+    Point center() const {return x;}
+    void move(Point to){x = to;}
+
+    void draw() const;
+    void rotate(int) {}
+    private:
+    Point x;
+    int r;
+};
+
+class Smiley: public Circle{
+    public:
+    Smiley(Point p, int r) : Circle(p,r), mouth{nullptr} {}
+
+    ~Smiley() override
+    {
+        delete mouth; // delete the shape in the destructor
+        for(auto p:eyes) // delete the eyes of type Shape
+        delete p;
+    }
+
+
+// void move() override
+// void draw()
+
+void add_eye(Shape* s){ eyes.push_back(s);}
+
+private:
+Vector<Shape*> eyes;
+Shape* mouth;
+};
 
