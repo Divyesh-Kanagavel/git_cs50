adabf0f822840cca2582f0a18731b3855e8d6d8a
Divyesh Kanagavel <kanagdhivy@gmail.com>
2022-07-22 16:25:19 +0530

    Added i/o cpp files

---
+++ tourcpp/iostr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+int main(){
+std::string s;
+
+std::cout << "Enter the name \n";
+std::getline(std::cin,s);
+
+std::cout << "The name is \n" << s ;
+
+
+return 0;
+}
+
+
